6038920b2b293c21c1af2bcb479767093a54219f
Jeniffer Orosco <jeniffer@gmail.com>
2026-06-18 15:58:46 -0500

    Agrego archivo tas

---
+++ tas.py
@@ -0,0 +1,19 @@
+# tas.py
+# Prueba básica SARIMAX
+
+print("Proyecto SARIMAX iniciado")
+
+import pandas as pd
+import numpy as np
+
+print("Librerías cargadas correctamente")
+
+# Datos de ejemplo
+datos = pd.Series(
+    [10, 12, 13, 15, 14, 18, 20, 22]
+)
+
+print("Datos:")
+print(datos)
+
+print("Fin de prueba")
